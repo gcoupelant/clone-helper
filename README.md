3dac3b65e320a0f4750719f4b0ea511e824f6d02
gcoupelant <guillaume.coupelant@ubisoft.com>
2021-02-11 18:11:19 +0800

    Add clone script

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Clone Helper
 
 Simple script to help clone repositories mapping the remote URL to a local path
+
+## Installation
+
+After cloning this repository, simply create an executable script in your `$PATH`, passing all argument to the `clone.py` file.
+
+For example, from the cloned repository directory, you can run:
+
+```bash
+$ echo "$PWD/clone.py \$@" | sudo tee /usr/local/bin/clone
+$ sudo chmod +x /usr/local/bin/clone
+```
+
+### Root of your source directory
+
+By default, the script uses `~/src` as the root directory for cloning. You can change this by editing the script's `SRC_PATH` constant.
+
+## Usage
+
+The script will use the repository hostname, path and name to construct the target directory.
+
+For example, cloning https://github.com/gcoupelant/clone-helper:
+
+```bash
+$ clone git@github.com:gcoupelant/clone-helper.git
+```
+
+This will clone the repository to `~/src/github.com/gcoupelant/clone-helper`.
---
+++ clone.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+import argparse
+import os
+import re
+import subprocess
+from operator import itemgetter
+from pathlib import Path
+
+SRC_PATH = "~/src"
+
+# Create the regex for the url
+git_url_re = re.compile(
+    r"^(?:git@|https?:\/\/)(?P<host>\S+?)(?::|\/)(?P<path>\S+?)(?:.git)?$")
+
+
+# Create the type parser
+def git_url_type(arg_value):
+    match = git_url_re.search(arg_value)
+    if not match:
+        raise argparse.ArgumentTypeError("invalid git url")
+    return match
+
+
+# Create the parser
+my_parser = argparse.ArgumentParser(
+    description=
+    f'Clone a repository in {SRC_PATH}, creating any intermediate directory')
+
+# Add the arguments
+my_parser.add_argument('url', type=git_url_type, help='the git url to clone')
+
+# Execute the parse_args() method
+args = my_parser.parse_args()
+
+# Extract info from the url
+git_url = args.url.string
+git_url_host, git_url_path = itemgetter('host', 'path')(args.url.groupdict())
+clone_path = Path(os.path.expanduser(SRC_PATH), git_url_host, git_url_path)
+
+# Check if directory already exist
+# TODO: Check if the directory is empty, since git can clone in that case
+if clone_path.exists():
+    raise SystemExit(
+        f'Error: target directory already exists.\n\nAccess it using:\ncd {clone_path}'
+    )
+
+# Clone the repository
+subprocess.check_call(['git', 'clone', git_url, clone_path])
+
+# Print helper to `cd` in the directory
+print(f'\nAccess cloned directory:\ncd {clone_path}')
