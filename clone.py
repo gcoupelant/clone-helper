4f2154f5d55be869e2c8390be7063c6d63dae87a
Guillaume Coupelant <guillaume.coupelant@tpicap.com>
2024-12-23 09:44:09 +0800

    Replace single quote by double quotes

--- clone.py
+++ clone.py
@@ -26,7 +26,7 @@ def git_url_type(arg_value):
 # Create the parser
 my_parser = argparse.ArgumentParser(
     description=
-    f'Clone a repository in {SRC_PATH}, creating any intermediate directory')
+    f"Clone a repository in {SRC_PATH}, creating any intermediate directory")
 
 # Add the arguments
 my_parser.add_argument("url", type=git_url_type, help="the git url to clone")
@@ -36,22 +36,22 @@ my_parser.add_argument("-q", "--quiet", action="store_true")
 args = my_parser.parse_args()
 
 # Extract info from the url
-git_url_host, git_url_path = itemgetter('host', 'path')(args.url.groupdict())
+git_url_host, git_url_path = itemgetter("host", "path")(args.url.groupdict())
 clone_path = Path(os.path.expanduser(SRC_PATH), git_url_host, git_url_path)
 
 # Check if directory already exist
 # TODO: Check if the directory is empty, since git can clone in that case
 if clone_path.exists():
     if not args.quiet:
-        print('Error: target directory already exists.\n\nAccess it using:', file=sys.stderr)
+        print("Error: target directory already exists.\n\nAccess it using:", file=sys.stderr)
     print(f"cd {clone_path}")
     raise SystemExit()
 
 # Clone the repository
-git_clone_url = f'git@{git_url_host}:{git_url_path}.git'
-subprocess.check_call(['git', 'clone', git_clone_url, clone_path])
+git_clone_url = f"git@{git_url_host}:{git_url_path}.git"
+subprocess.check_call(["git", "clone", git_clone_url, clone_path])
 
 # Print helper to `cd` in the directory
 if not args.quiet:
-    print('\nAccess cloned directory:')
-print(f'cd {clone_path}')
+    print("\nAccess cloned directory:")
+print(f"cd {clone_path}")
