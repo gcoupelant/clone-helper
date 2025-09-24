b3c7f278c1d0c7fd1d3ea52522d28491cf05a6e1
Guillaume Coupelant <guillaume.coupelant@gmail.com>
2025-09-24 11:53:13 +0800

    Allow cloning in empty directories

--- clone.py
+++ clone.py
@@ -24,28 +24,29 @@ def git_url_type(arg_value):
 
 
 # Create the parser
-my_parser = argparse.ArgumentParser(
+parser = argparse.ArgumentParser(
     description=
     f"Clone a repository in {SRC_PATH}, creating any intermediate directory")
 
 # Add the arguments
-my_parser.add_argument("url", type=git_url_type, help="the git url to clone")
-my_parser.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("url", type=git_url_type, help="the git url to clone")
+parser.add_argument("-q", "--quiet", action="store_true")
 
 # Execute the parse_args() method
-args = my_parser.parse_args()
+args = parser.parse_args()
 
 # Extract info from the url
 git_url_host, git_url_path = itemgetter("host", "path")(args.url.groupdict())
 clone_path = Path(os.path.expanduser(SRC_PATH), git_url_host, git_url_path)
 
 # Check if directory already exist
-# TODO: Check if the directory is empty, since git can clone in that case
 if clone_path.exists():
-    if not args.quiet:
-        print("Error: target directory already exists.\n\nAccess it using:", file=sys.stderr)
-    print(f"cd {clone_path}")
-    raise SystemExit()
+    # Check if the directory is empty, since git can clone in that case
+    if any(clone_path.iterdir()):
+        if not args.quiet:
+            print("Error: target directory already exists and is not empty.\n\nAccess it using:", file=sys.stderr)
+        print(f"cd {clone_path}")
+        raise SystemExit()
 
 # Clone the repository
 git_clone_url = f"git@{git_url_host}:{git_url_path}.git"
